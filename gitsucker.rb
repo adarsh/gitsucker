ff7acc2557d71cb9af80438d49976ffcf1d9f19f
Adarsh Pandit <adarshpandit@gmail.com>
2012-04-24 14:51:53 -0400

    Now searches repo to determine author name

--- gitsucker.rb
+++ gitsucker.rb
@@ -1,6 +1,8 @@
 #!/usr/bin/env ruby
+require 'rubygems'
 require 'net/http'
 require 'open-uri'
+require 'nokogiri'
 require 'json'
 
 class GithubRepo
@@ -8,16 +10,29 @@ class GithubRepo
   GIT_REPO_SEARCH_URL = 'https://github.com/api/v2/xml/repos/search/'
   GIT_API_URL = 'https://api.github.com/repos/'
 
+  # Methods
   def initialize(repo)
-    @repo = repo
-    @author = 'thoughtbot'
+    repo = repo
+    author = determine_author(repo)
+    fetch_repo_data(repo, author)
   end
 
   def number_of_repos
-    url = GIT_API_URL + @author + "/" + @repo
+    @repo_data["forks"]
+  end
+
+  private
+
+  def determine_author(query)
+    url = GIT_REPO_SEARCH_URL + query
+    results = Nokogiri::XML(open(url))
+    first_search_result_username = results.css("username").first.content
+  end
+
+  def fetch_repo_data(repo, author)
+    url = GIT_API_URL + author + "/" + repo
     content = open(url).read
-    data = JSON.parse(content)
-    data["forks"]
+    @repo_data = JSON.parse(content)
   end
 end
 
