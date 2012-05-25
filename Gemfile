27904abdedc5e4d2c036ada2273773a63bb50272
Gabe Berke-Williams <gabe@thoughtbot.com>
2012-05-24 21:03:19 -0400

    Add spec for Repo.

--- Gemfile
+++ Gemfile
@@ -3,3 +3,4 @@ source :rubygems
 gem 'nokogiri'
 gem 'json'
 gem 'rspec', '~> 2.9.0'
+gem 'mocha'
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,4 +3,5 @@ $LOAD_PATH << File.expand_path("../lib", __FILE__)
 require 'gitsucker'
 
 RSpec.configure do |c|
+  c.mock_with :mocha
 end
